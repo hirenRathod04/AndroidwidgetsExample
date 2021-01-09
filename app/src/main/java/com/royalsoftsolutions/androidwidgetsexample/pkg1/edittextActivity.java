742e1ce9b41dcbb47575fb25c74f2d83de26434a
Hiren <rs.hirenrathod@gmail.com>
2021-01-09 17:30:41 +0530

    Initial commit

---
+++ app/src/main/java/com/royalsoftsolutions/androidwidgetsexample/pkg1/edittextActivity.java
@@ -0,0 +1,16 @@
+package com.royalsoftsolutions.androidwidgetsexample.pkg1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.royalsoftsolutions.androidwidgetsexample.R;
+
+public class edittextActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edittext);
+    }
+}
